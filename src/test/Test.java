b43f8b775fc54ec63ec62cfd41f4b346b36a0e32
helgamartens0 <helgamartens@icloud.com>
2024-03-20 18:08:02 -0300

    helga escribio

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,8 +2,12 @@ package test;
 
 public class Test {
 
+    
     public static void main(String[] args) {
-
+        
         System.out.println("Lautaro Petelin");
+        System.out.println("Erick Bunino");
+        System.out.println("Helga Martens");
+
     }
 }
